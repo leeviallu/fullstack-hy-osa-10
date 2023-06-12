cbae38b7f3b7ef94000065bf8c726e755ee58865
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-06-12 23:47:44 +0300

    useNavigate added for signing in

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -2,6 +2,7 @@ import * as yup from "yup";
 import { Formik } from "formik";
 import SignInForm from "./SignInForm";
 import useSignIn from "../hooks/useSignIn";
+import { useNavigate } from "react-router-native";
 
 const validationSchema = yup.object().shape({
     username: yup
@@ -16,12 +17,14 @@ const validationSchema = yup.object().shape({
 
 const SignIn = () => {
     const [signIn] = useSignIn();
+    const navigate = useNavigate();
 
     const onSubmit = async (values) => {
         const { username, password } = values;
         try {
             const { data } = await signIn({ username, password });
             console.log(data);
+            navigate("/");
         } catch (e) {
             console.log(e);
         }
