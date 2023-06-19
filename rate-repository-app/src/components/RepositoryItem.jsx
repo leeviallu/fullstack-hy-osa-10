6366f815d45e8b2da884d6196a8f1a0a13b6dcca
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-06-19 22:28:06 +0300

    Repository reviews ready

--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -11,6 +11,7 @@ import theme from "../theme";
 import useRepository from "../hooks/useRepository";
 import useReviews from "../hooks/useReviews";
 import { useParams } from "react-router-native";
+import { format } from "date-fns";
 
 const styles = StyleSheet.create({
     container: {
@@ -266,12 +267,33 @@ const ReviewItem = ({ review }) => {
             }}
         >
             <View>
-                <Text>{review.node.rating}</Text>
+                <View
+                    style={{
+                        margin: 15,
+                        width: 50,
+                        height: 50,
+                        borderWidth: 3,
+                        borderRadius: 30,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        borderColor: theme.colors.primary,
+                    }}
+                >
+                    <Text
+                        style={{
+                            color: theme.colors.primary,
+                        }}
+                    >
+                        {review.node.rating}
+                    </Text>
+                </View>
             </View>
-            <View>
-                <Text>{review.node.user.username}</Text>
-                <Text>{review.node.createdAt}</Text>
-                <Text>{review.node.text}</Text>
+            <View style={{ paddingVertical: 10, width: "75%" }}>
+                <Text fontWeight="bold">{review.node.user.username}</Text>
+                <Text style={{ paddingTop: 5, color: "gray" }}>
+                    {format(new Date(review.node.createdAt), "dd.MM.yyyy")}
+                </Text>
+                <Text style={{ paddingTop: 5 }}>{review.node.text}</Text>
             </View>
         </View>
     );
@@ -289,11 +311,10 @@ const SingleRepository = () => {
         <FlatList
             data={reviews}
             renderItem={({ item }) => <ReviewItem review={item} />}
-            keyExtractor={({ id }) => id}
+            keyExtractor={({ node }) => node.id}
             ListHeaderComponent={() => (
                 <RepositoryInfo repository={repository} />
             )}
-            // ...
         />
     );
 };
